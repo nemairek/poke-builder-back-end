a22ff0b00fe05f5b169f703d7d73362be7641bb7
Knemaire <kannemaire@gmail.com>
2024-08-26 17:03:17 -0400

    init

---
+++ controllers/pokebuilder.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const verifyToken = require('../middleware/verify-token.js');
+const Pokebuilder = require('../models/pokebuilder.js');
+
+const router = express.Router();
+
+
+
+router.get('/search/:pokename', async (req, res) => {
+    console.log(req.params.pokename)
+    try {
+
+        // const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu/`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${req.params.pokename}`);
+        const pokemon = await response.json();
+        // const bioResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`);
+        // const pokemonBio = await bioResponse.json();
+        console.log(pokemonBio)
+       res.status(200).json(pokemon)
+    } catch (error) {
+      res.status(500).json(error);
+    }
+});
+
+module.exports = router
---
+++ models/pokebuilder.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    },
+    { timestamps: true }
+  );
+
+
+const pokebuilderSchema = new mongoose.Schema(
+    {
+        id: {
+            type: Number,
+            required: true,
+        },
+      name: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+      image: String, 
+      ownedBy: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+      }],
+      comments: [commentSchema]
+    },
+    { timestamps: true }
+  );
+
+
+
+  
+  const Pokebuilder = mongoose.model('Pokebuilder', pokebuilderSchema);
+
+  module.exports = Pokebuilder;
